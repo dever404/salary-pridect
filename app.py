fdbecbc051af068da95cd0c6e15f7cc8240e16a0
dever <abdo.errafaiy@gmail.com>
2024-02-04 16:26:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 import pandas as pd
 import streamlit as st 
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OrdinalEncoder
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn import preprocessing
 
 def main(): 
-    #st.title("Jobs slary pridect")
     html_temp = """
     <div style="background:#025246 ;padding:10px; margin-bottom:20px">
-    <h2 style="color:white;text-align:center;">Jobs slary pridect App </h2>
+    <h2 style="color:white;text-align:center;">Predictive jobs salary </h2>
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html = True)
@@ -30,18 +26,22 @@ def main():
         X = salary.drop(['salary'], axis=1)
         y = salary['salary']
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
         lm = LinearRegression()
 
         ordinal_inco=OrdinalEncoder()
-        x_train=ordinal_inco.fit_transform(X_train)
+        x_train=ordinal_inco.fit_transform(X)
         x_test=ordinal_inco.fit_transform(input_data)
         
-        lm.fit(x_train, y_train)
+        lm.fit(x_train, y)
         
         predicted_salary = lm.predict(x_test)
 
         st.success('Predicted Salary for Input Data : {}'.format( predicted_salary[0]))
+
+    footer = """
+    <footer style="text-align:center;margin-top:20px"> Developer by : Abdelhakim ER-RAFAIY , Source code <a href="https://github.com/dever404/salary-pridect" target="_blank">GitHub</a></footer>
+    """
+    st.markdown(footer, unsafe_allow_html = True)
 
 if __name__=='__main__': 
     main() 
